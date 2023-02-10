b09929dfe2baa2d9728a495b3b553460fc4110a2
Geza Czimeth <gezaczimeth@gmail.com>
2023-02-10 08:54:04 +0100

    Rest interface added for order creation

---
+++ microservices/base-domain/src/main/java/com/bigfish/dto/OrderDto.java
@@ -0,0 +1,6 @@
+package com.bigfish.dto;
+
+public class OrderDto {
+    public String product;
+    public int quantity;
+}
---
+++ microservices/order-microservice/src/main/java/com/bigfish/order/controller/OrderServiceController.java
@@ -0,0 +1,21 @@
+package com.bigfish.order.controller;
+
+import com.bigfish.dto.OrderDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+@Slf4j
+public class OrderServiceController {
+
+    @PostMapping()
+    public ResponseEntity<OrderDto> order(@RequestBody OrderDto orderDto) {
+        log.info(String.format("Order service received order -> %s", orderDto));
+        return ResponseEntity.ok(orderDto);
+    }
+}
